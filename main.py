ba1f68517c08687ec95e7ae108e58f2ca2e39363
gabriela-bianchini <gabi.b.bianchini@gmail.com>
2026-08-30 18:19:07 -0300

    Add multiplication and division functions

--- main.py
+++ main.py
@@ -5,10 +5,14 @@ def calcula_soma(x,y):
 def calcula_subtração(x,y):
   return x - y
 
+def calcula_multiplicação(x,y):
+  return x * y
+
+def calcula_divisão(x,y):
+  return x / y
+
 def calcula_resto(x,y):
   return x % y
 
 def calcula_percentual(x,y):
   return x * y / 100
-
-
